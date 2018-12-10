8e26689320adb7aa74e70db18faa7032c18ae89e
Gabriel Busq <busquim10@MacBook-Air-4.local>
2018-12-10 00:44:51 -0200

    criacao de novo html

--- inkless/views.py
+++ inkless/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
+    data= {}
+    data['nome']=['t1','t2','t1']
+    return render(request, 'inkless/home.html',data)
 
-    return render(request, 'inkless/home.html')
+def teste(request):
+    return render(request, 'inkless/teste.html')
--- inkless_site/urls.py
+++ inkless_site/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from inkless.views import home
+from inkless.views import home,teste
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',home) 
+    path('',home),
+    path('teste/',teste) 
 ]
